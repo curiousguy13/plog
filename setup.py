b0a08e9b10db808b0d6a103162e4aab78b5f73bc
Daniel Narvaez <dwnarvaez@gmail.com>
2013-05-22 23:32:46 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name="plog",
       packages=["plog"],
-      version="0.2",
+      version="0.3",
       description="Logged process",
       author="Daniel Narvaez",
       author_email="dwnarvaez@gmail.com",
